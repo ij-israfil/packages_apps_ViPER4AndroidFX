a9d41f944c1cfa4632345992a5761e8c72b1b969
Ij Israfil <israfilkhan494@gmail.com>
2026-01-20 02:11:39 +0000

    v4a: convert Android.mk to Android.bp
    * overrides AudioFX & MusicFX
    * Move viperfx app to system-ext
    
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>

--- config.mk
+++ config.mk
@@ -1,7 +1,3 @@
-BUILD_PATH := packages/apps/ViPER4AndroidFX
-
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(BUILD_PATH)/vendor,vendor)
-
 PRODUCT_PACKAGES += \
-    ViPER4AndroidFX
+    ViPER4AndroidFX \
+    libv4a_fx
